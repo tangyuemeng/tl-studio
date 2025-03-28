86915471a38d26c4e81c2b69cd57aa91cd29e39d
tangyuemeng <tangyuemeng@gmail.com>
2025-03-28 23:25:35 +0900

    commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,114 +1,44 @@
 <template>
-    <header class="header" ref="header">
-      <div class="header-backgroud"></div>
-      <div class="header-container">
-        <img class="logo" src="../assets/img/logo.svg"></img>
-        <nav class="navigation ">
-          <slot name="navigation">
-            <ul>
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#services">Services</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </slot>
-        </nav>
-      </div>
-    </header>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref } from "vue";
-  import gsap from "gsap";
-  import { ScrollTrigger } from "gsap/ScrollTrigger";
-  
-  gsap.registerPlugin(ScrollTrigger);
-  
-  const header = ref(null);
-  
-  onMounted(() => {
-    if (!header.value) {
-      console.error("🚨 `h1` 元素未正确加载");
-      return;
-    }
-  
-    gsap.to(header.value, {
-      maxWidth: "100vw",
-      backgroundColor: "rgba(255, 255, 255, 0.09)",
-      ease: "power2.out",
-      scrollTrigger: {
-        scroller: document.documentElement,
-        start: "top top",
-        end: "+=60vh",
-        scrub: 1,
-      },
-    });
+  <header class="bg-transparent text-white p-4 flex justify-between w-screen sticky top-0 z-10" ref="header">
+    <img class="h-5 md:h-10 w-auto" src="../assets/logo.svg"></img>
+    <nav class="mr-20 mt-2 hidden md:flex">
+      <ul class="flex text-xl font-sans font-bold">
+        <li class="ml-8"><a href="#home">About</a></li>
+        <li class="ml-8"><a href="#about">Schedule</a></li>
+        <li class="ml-8"><a href="#services">Services</a></li>
+        <li class="ml-8"><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const header = ref(null);
+
+onMounted(() => {
+  if (!header.value) {
+    console.error("🚨 `h1` 元素未正确加载");
+    return;
+  }
+
+  gsap.to(header.value, {
+    maxWidth: "100vw",
+    backgroundColor: "rgba(255, 255, 255, 0.09)",
+    ease: "power2.out",
+    scrollTrigger: {
+      scroller: document.documentElement,
+      start: "top top",
+      end: "+=60vh",
+      scrub: 1,
+    },
   });
-  </script>
-  
-  <style scoped>
-  .header {
-    position: sticky;
-    z-index: 1000;
-    top: 0;
-    max-width: 980px;
-    margin: 0 auto;
-    border-bottom: 1px solid var(--divider-color);
-    transition: width 0.3s ease-out;
-  }
-  
-  .header-backgroud {
-    backdrop-filter: saturate(180%) blur(20px);
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-  }
-  
-  .header-container {
-    position: relative;
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 0 0 0.5rem;
-    z-index: 2;
-    margin: 0 auto;
-    max-width: 980px;
-  }
-  
-  .logo {
-    width: auto;
-    height: 2rem;
-    opacity: 0.92;
-    padding-top: 10px;
-  }
-  
-  .navigation {
-    padding-top: 20px;
-  }
-  
-  .navigation ul {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-  
-  .navigation li {
-    margin-left: 1.5rem;
-    font-size: 0.8rem;
-  }
-  
-  .navigation a {
-    text-decoration: none;
-    color: var(--text-normal-color);
-    line-height: 1;
-    font-weight: 400;
-  }
-  
-  .navigation a:hover {
-    color: var(--text-active-color);
-  }
-  </style>
+});
+</script>
+
+<style scoped></style>
--- src/components/views/HomePage.vue
+++ src/components/views/HomePage.vue
@@ -1,12 +1,15 @@
 <template>
-    <Header />
+    <div class="w-screen h-screen">
+        <Header />
+        <video autoplay loop muted playsinline class="absolute top-0 left-0 w-full h-screen object-cover">
+      <source src="../../assets/background.mp4" type="video/mp4" />
+    </video>
+    </div>
 </template>
-  
+
 <script setup>
-import Header from '../Header.vue' 
+import Header from '../Header.vue'
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
