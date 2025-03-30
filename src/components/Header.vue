f33ace020fe3dc8cca0987081e2eac4eb2d3b0b0
tangyuemeng <tangyuemeng@gmail.com>
2025-03-30 17:04:20 +0900

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="bg-transparent text-white p-5 flex justify-between w-screen sticky top-0 z-10" ref="header">
-    <img class="h-[30px] md:h-[42px] w-auto ml-2 md:ml-10" src="../assets/logo.svg"></img>
+    <img class="h-[30px] md:h-[42px] w-auto ml-2 md:ml-10" src="/logo-white.svg"></img>
     <nav class="mr-30 mt-2 hidden md:flex">
       <ul class="flex text-xl font-sans font-bold">
         <li class="ml-10"><a href="#home">About</a></li>
