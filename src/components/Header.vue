daba9d011510f32070f6a2d9a64f531d45b71712
tangyuemeng <tangyuemeng@gmail.com>
2025-03-30 19:13:52 +0900

    111

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,14 @@
 <template>
   <header class="bg-transparent text-white p-5 flex justify-between w-screen sticky top-0 z-10" ref="header">
-    <img class="h-[30px] md:h-[42px] w-auto ml-2 md:ml-10" src="/logo-white.svg"></img>
+    <router-link to="/">
+      <img class="h-[30px] md:h-[42px] w-auto ml-2 md:ml-10" src="/logo-white.svg" alt="Logo" />
+    </router-link>
     <nav class="mr-30 mt-2 hidden md:flex">
       <ul class="flex text-xl font-sans font-bold">
-        <li class="ml-10"><a href="#home">About</a></li>
-        <li class="ml-10"><a href="#about">Schedule</a></li>
-        <li class="ml-10"><a href="#services">Class</a></li>
-        <li class="ml-10"><a href="#contact">Contact</a></li>
+        <li class="ml-10"><router-link to="/home">About</router-link></li>
+        <li class="ml-10"><router-link to="/schedule">Schedule</router-link></li>
+        <li class="ml-10"><router-link to="/class">Class</router-link></li>
+        <li class="ml-10"><router-link to="/contact">Contact</router-link></li>
       </ul>
     </nav>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../components/views/HomePage.vue";
+import SchedulePage from "../components/views/SchedulePage.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: HomePage,
+  },
+  {
+    path: "/schedule",
+    name: "Schedule",
+    component: SchedulePage,
   },
 ];
 
