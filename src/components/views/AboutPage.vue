08dd9f190d81c31648389f9946a79a42b443c943
YUEMENG TANG <yuemengtang@PC180.local>
2025-04-05 17:44:35 +0900

    fix all

--- src/components/views/AboutPage.vue
+++ src/components/views/AboutPage.vue
@@ -1,13 +1,27 @@
 <template>
-    <Header />
+        <Header />
+        <div class="w-full flex flex-col mt-[4.5rem] bg-cover bg-center" style="background-image: url('https://firebasestorage.googleapis.com/v0/b/tl-studio-2a05d.firebasestorage.app/o/web_images%2Fbg.png?alt=media&token=a6d01042-6e0a-4b7e-b90e-ad97c0fb881c')">
+            <div class="md:text-5xl text-4xl text-center flex flex-col justify-center font-bold py-2 mt-[3rem]">WE CREATE
+                <b class="md:text-8xl text-5xl py-2">COMMUNITY</b>
+                <div class="flex flex-col md:flex-row px-4 py-2 mt-4 justify-center items-center">
+                    <img class="w-full md:w-1/2 h-auto" src="https://firebasestorage.googleapis.com/v0/b/tl-studio-2a05d.firebasestorage.app/o/web_images%2Fbg-2.png?alt=media&token=b39cb55c-ef8e-4729-b6ce-1478bd67291b">
+                    <p class="text-base w-full md:w-1/3 md:ml-8 mt-4 text-start">{{ t('studioIntroduction')}}</p>
+                </div>
+            </div>
+        </div>
+        <div class="x-4 py-2 bg-white text-center font-bold text-black text-xl">
+            <router-link to="/schedule">Check Today Schedule?</router-link>
+        </div>
 
-    <Footer />
+        <Footer />
 </template>
 
 <script setup>
 import Header from '../Header.vue'
 import Footer from '../Footer.vue';
 import { ref, onMounted } from 'vue';
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
 
 onMounted(() => { });
 </script>
--- src/components/views/ClassPage.vue
+++ src/components/views/ClassPage.vue
@@ -3,33 +3,37 @@
     <div class="md:text-8xl font-bold text-center px-4 pt-10 text-5xl mt-[4.5rem]">CLASS LINEUP</div>
     <div class="md:text-3xl font-bold text-center px-4 pb-10 text-xl mt-4">{{ t('classType') }}</div>
     <div
-        class="flex flex-col w-full md:flex-row mx-4 md:mx-0 border-l-2 border-gray-300/30 md:border-none md:items-center md:justify-center md:gap-10">
-        <div class="flex flex-col relative mb-[20px]">
+        class="flex flex-col w-full md:flex-row pl-4 md:mx-0 md:items-center md:justify-center md:gap-10 mb-20">
+        <div class="flex flex-col relative">
             <div class="absolute bg-sky-400 w-[10px] h-[10px] rounded-full -left-[6px] top-[10px]"></div>
+            <div class="absolute bg-gray-300/30 w-[2px] h-full -left-[2px] top-[15px] -z-10 md:hidden"></div>
             <b class="font-bold text-xl mb-[4px] ml-4">超入门</b>
             <div class="ml-4 text-sm">{{ t('beginnerTitle') }}</div>
             <img class="w-[272px] h-[172px] bg-white object-cover mt-[30px] ml-auto mr-[30px] mb-4"
                 src="https://firebasestorage.googleapis.com/v0/b/tl-studio-2a05d.firebasestorage.app/o/web_images%2Fclasses%2F1.png?alt=media&token=5729e7cf-9893-4376-ab40-83ab3a1f0d12">
         </div>
 
-        <div class="flex flex-col relative mb-[20px]">
+        <div class="flex flex-col relative">
             <div class="absolute bg-green-400 w-[10px] h-[10px] rounded-full -left-[6px] top-[10px]"></div>
+            <div class="absolute bg-gray-300/30 w-[2px] h-full -left-[2px] top-[15px] -z-10 md:hidden"></div>
             <b class="font-bold text-xl mb-[4px] ml-4">初级</b>
             <div class="ml-4 text-sm">{{ t('basicTitle') }}</div>
             <img class="w-[272px] h-[172px] bg-white object-cover mt-[30px] ml-auto mr-[30px] mb-4"
             src="https://firebasestorage.googleapis.com/v0/b/tl-studio-2a05d.firebasestorage.app/o/web_images%2Fclasses%2F3.png?alt=media&token=69fa73d5-1495-49ab-bbf4-4aea5e0427ae">
         </div>
 
-        <div class="flex flex-col relative mb-[20px]">
+        <div class="flex flex-col relative">
             <div class="absolute bg-orange-400 w-[10px] h-[10px] rounded-full -left-[6px] top-[10px]"></div>
+            <div class="absolute bg-gray-300/30 w-[2px] h-full -left-[2px] top-[15px] -z-10 md:hidden"></div>
             <b class="font-bold text-xl mb-[4px] ml-4">中级</b>
             <div class="ml-4 text-sm">{{ t('intermediateTitle') }}</div>
             <img class="w-[272px] h-[172px] bg-white object-cover mt-[30px] ml-auto mr-[30px] mb-4"
             src="https://firebasestorage.googleapis.com/v0/b/tl-studio-2a05d.firebasestorage.app/o/web_images%2Fclasses%2F2.png?alt=media&token=dbb7d5c3-d35c-4288-ac05-7e9f274276fc">
         </div>
 
-        <div class="flex flex-col relative mb-[20px]">
+        <div class="flex flex-col relative">
             <div class="absolute bg-indigo-400 w-[10px] h-[10px] rounded-full -left-[6px] top-[10px]"></div>
+            <div class="absolute bg-gray-300/30 w-[2px] h-full -left-[2px] top-[15px] -z-10 md:hidden"></div>
             <b class="font-bold text-xl mb-[4px] ml-4">Pop Up</b>
             <div class="ml-4 text-sm">{{ t('popupTitle') }}</div>
             <img class="w-[272px] h-[172px] bg-white object-cover mt-[30px] ml-auto mr-[30px] mb-4" 
