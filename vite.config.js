be9e97aed9e1c3bac41cd194aef591b85e62a06d
YUEMENG TANG <yuemengtang@PC180.local>
2025-04-05 18:15:11 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
+
+const routes = [
+  "/",
+  "/schedule",
+  "/aboutus",
+  "/contact",
+  "/class",
+  "/faq",
+  "/terms-of-service",
+  "/privacy-policy",
+];
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
-  publicPath: process.env.NODE_ENV === 'production' ? '/tl-studio/' : '/'
-})
+  plugins: [
+    vue(),
+    tailwindcss(),
+    Sitemap({
+      hostname: "https://tldancestudio.com",
+      routes,
+    }),
+  ],
+  publicPath: process.env.NODE_ENV === "production" ? "/tl-studio/" : "/",
+});
